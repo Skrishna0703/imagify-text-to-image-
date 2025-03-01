e91fc365d58ab011e7dd841168964d50486dc07f
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-03-01 10:04:51 +0530

    style

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  mongoose.connection.on("connected", () => {
+    console.log("Database Connected");
+  });
 
-    mongoose.connection.on('connected', () => {
-        console.log("Database Connected");
-    })
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/Pixelize`)
-
-}
+  await mongoose.connect(`${process.env.MONGODB_URI}/Pixelize`);
+};
 
 export default connectDB;
--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,52 +1,64 @@
-import axios from 'axios'
-import fs from 'fs'
-import FormData from 'form-data'
-import userModel from '../models/userModel.js'
+import axios from "axios";
+import fs from "fs";
+import FormData from "form-data";
+import userModel from "../models/userModel.js";
 
 // Controller function to generate image from prompt
 // http://localhost:4000/api/image/generate-image
 export const generateImage = async (req, res) => {
-
   try {
-
-    const { userId, prompt } = req.body
+    const { userId, prompt } = req.body;
 
     // Fetching User Details Using userId
-    const user = await userModel.findById(userId)
-    
+    const user = await userModel.findById(userId);
+
     if (!user || !prompt) {
-      return res.json({ success: false, message: 'Missing Details' })
+      return res.json({ success: false, message: "Missing Details" });
     }
 
     // Checking User creditBalance
     if (user.creditBalance === 0 || userModel.creditBalance < 0) {
-      return res.json({ success: false, message: 'No Credit Balance', creditBalance: user.creditBalance })
+      return res.json({
+        success: false,
+        message: "No Credit Balance",
+        creditBalance: user.creditBalance,
+      });
     }
 
     // Creation of new multi/part formdata
-    const formdata = new FormData()
-    formdata.append('prompt', prompt)
+    const formdata = new FormData();
+    formdata.append("prompt", prompt);
 
     // Calling Clipdrop API
-    const { data } = await axios.post('https://clipdrop-api.co/text-to-image/v1', formdata, {
-      headers: {
-        'x-api-key': process.env.CLIPDROP_API,
-      },
-      responseType: "arraybuffer"
-    })
+    const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formdata,
+      {
+        headers: {
+          "x-api-key": process.env.CLIPDROP_API,
+        },
+        responseType: "arraybuffer",
+      }
+    );
 
     // Convertion of arrayBuffer to base64
-    const base64Image = Buffer.from(data, 'binary').toString('base64');
-    const resultImage = `data:image/png;base64,${base64Image}`
+    const base64Image = Buffer.from(data, "binary").toString("base64");
+    const resultImage = `data:image/png;base64,${base64Image}`;
 
-    // Deduction of user credit 
-    await userModel.findByIdAndUpdate(user._id, { creditBalance: user.creditBalance - 1 })
+    // Deduction of user credit
+    await userModel.findByIdAndUpdate(user._id, {
+      creditBalance: user.creditBalance - 1,
+    });
 
     // Sending Response
-    res.json({ success: true, message: "Background Removed", resultImage, creditBalance: user.creditBalance - 1 })
-
+    res.json({
+      success: true,
+      message: "Background Removed",
+      resultImage,
+      creditBalance: user.creditBalance - 1,
+    });
   } catch (error) {
-    console.log(error.message)
-    res.json({ success: false, message: error.message })
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
   }
-}
+};
